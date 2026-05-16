a1b56ef3f829c346a748428c4f1fb26cfeba5c5c
Wil <wil.lsr@outlook.com>
2026-05-16 15:06:36 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Carine meu amor, você é uma pessoa maravilhosa, única, com um jeito especial que só você tem, te admiro muito! Obrigado por me permitir compartilhar a vida com você, beijos meu amor ♥</h1>;
+}
+
+export default Home;
